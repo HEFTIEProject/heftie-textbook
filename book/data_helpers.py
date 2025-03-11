7d078e392b09d866be63d6857eb82b096cb82120
David Stansby <dstansby@gmail.com>
2025-03-11 17:47:15 +0000

    Use zarr v2

--- book/data_helpers.py
+++ book/data_helpers.py
@@ -7,7 +7,9 @@ import numpy as np
 
 def load_heart_data(array_type: Literal["numpy"]):
     arr_zarr = zarr.open_array(
-        store=zarr.storage.LocalStore(Path(__file__).parent / "data" / "hoa_heart.zarr")
+        store=zarr.storage.DirectoryStore(
+            Path(__file__).parent / "data" / "hoa_heart.zarr"
+        )
     )
     if array_type == "numpy":
         return np.asarray(arr_zarr[:])
